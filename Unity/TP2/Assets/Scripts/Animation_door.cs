99f7320132af0fa048da1409388d0eba44ab2017
ALX5 <alexislinke@hotmail.fr>
2013-10-15 14:48:17 +0200

    TP 2-3 Unity.

---
+++ Unity/TP2/Assets/Scripts/Animation_door.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animation_door : MonoBehaviour {
+	
+	public GameObject gameObject;
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition)))
+			if(Input.GetMouseButton(0)) {			
+				gameObject.animation.Play("door-open");
+			}	
+			else if(Input.GetMouseButton(1)) {
+				gameObject.animation.Play("door-close");
+			}	
+	}
+}
---
+++ Unity/TP2/Assets/Scripts/Camera_movement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_movement : MonoBehaviour {
+	
+	public GameObject gameObject;
+	private Transform transform;
+	
+	// Use this for initialization
+	void Start () {
+		transform = gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.UpArrow))
+			transform.Translate(0,0,(float)0.2);
+		else if(Input.GetKey(KeyCode.DownArrow))
+			transform.Translate(0,0,(float)-0.2);
+		else if(Input.GetKey(KeyCode.RightArrow))
+			transform.Rotate(0,1,0);
+		else if(Input.GetKey(KeyCode.LeftArrow))
+			transform.Rotate(0,-1,0);
+		else if (Input.GetAxis("Mouse ScrollWheel") != 0)
+			transform.Rotate(Input.GetAxis("Mouse ScrollWheel")*50,0,0);
+	}
+}
